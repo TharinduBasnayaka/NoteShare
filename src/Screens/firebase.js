48bf7e01089a3fe416a9d1618d29d9a9d2eee382
Tharindu Basanayaka <tbasnayaka7@gmail.com>
2020-08-02 15:21:14 +0530

    can uploads file to the storage
    also setting the download links to database

--- src/Screens/firebase.js
+++ src/Screens/firebase.js
@@ -1,10 +1,11 @@
 /* eslint-disable prettier/prettier */
-import firebase from '@react-native-firebase/app';
+import firebase from 'firebase';
 
 const config = {
-    apiKey: "<API_KEY>",
-    authDomain: "<PROJECT_ID>.firebaseapp.com",
-    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-    storageBucket: "<BUCKET>.appspot.com"
+    apiKey: "AIzaSyBln_60H8S2G6mBBtmKTfiVC1SPd7Ev-K0",
+    authDomain: "noteshare-871b5.firebaseapp.com",
+    databaseURL: "https://noteshare-871b5.firebaseio.com",
+    storageBucket: "noteshare-871b5.appspot.com"
+
 };
 firebase.initializeApp(config);
